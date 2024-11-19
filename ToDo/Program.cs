683024eee47da34d7b96b578cbaa05ecbd514b64
Isaac R <isaac.rebolledoleal@gmail.com>
2024-11-19 09:39:50 -0300

    changing default url

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -46,7 +46,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=ToDo}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
